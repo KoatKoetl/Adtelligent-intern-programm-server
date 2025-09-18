19f1e58636b2df9c9875008528725fee4a856172
Stanislav Suschevici <stas20027@outlook.com>
2025-09-18 21:52:36 +0300

    (Feature) - add prisma as plugin to be autoloaded

--- src/types/fastify.d.ts
+++ src/types/fastify.d.ts
@@ -1,8 +1,10 @@
+import type { PrismaClient } from "../../prisma/generated/prisma/client";
 import type { Config } from "../config/schema";
 
 declare module "fastify" {
 	interface FastifyInstance {
 		config: Config;
 		pluginLoaded: (pluginName: string) => void;
+		prisma: PrismaClient;
 	}
 }
